c9d7a2d8a562f4956be412b9839f0a835e07c5f6
Seth <samaritanbrotherseth@gmail.com>
2025-08-13 15:13:04 -0400

    Finally used the display: block to bring the divs together but now trying to figure out the button text-decoration feature.

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,22 +3,26 @@ import seth from "./seth.jpg";
 export default function Header() {
   return (
     <>
-      <div>
-        <img src={seth} alt="profile picture" class="profile-pic" />
-      </div>
-      <div className="top-half">
-        <br />
-        <h1>Seth Johnson</h1>
-        <h2>FullStack Developer</h2>
-        <h3>samaritanbrotherseth@gmail.com</h3>
-        <div className="buttons">
-          <button className="email">
-            <i className="fa-solid fa-envelope" id="email-envelope"></i>Email
-          </button>
-          <button className="linkedin">
-            <i className="fa-brands fa-square-linkedin" id="linkedin-in"></i>
-            linkedIn
-          </button>
+      <div className="container">
+        <img src={seth} alt="profile picture" className="profile-pic" />
+
+        <div className="top-half">
+          <br />
+          <h1>Seth Johnson</h1>
+          <h2>FullStack Developer</h2>
+          <h3>samaritanbrotherseth@gmail.com</h3>
+          <div className="buttons">
+            <button className="email">
+              <i className="fa-solid fa-envelope" id="email-envelope"></i>
+              <a href="https://mail.google.com/mail/u/0/#inbox">Email</a>
+            </button>
+            <button className="linkedin">
+              <i className="fa-brands fa-square-linkedin" id="linkedin-in"></i>
+              <a href="https://www.linkedin.com/in/seth-johnson-10a6a217b/">
+                linkedIn
+              </a>
+            </button>
+          </div>
         </div>
       </div>
     </>
