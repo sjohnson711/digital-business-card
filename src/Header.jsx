0c3dd6149dc386e62f69dbb341ba76583d7db71c
Seth <samaritanbrotherseth@gmail.com>
2025-08-13 09:05:53 -0400

    finally centered the Digital card now I need to close the gap between both divs

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,21 +2,25 @@ import seth from "./seth.jpg";
 
 export default function Header() {
   return (
-    <div className="top-half">
-      <img src={seth} alt="profile picture" class="profile-pic" />
-      <br />
-      <h1>Seth Johnson</h1>
-      <h2>FullStack Developer</h2>
-      <h3>samaritanbrotherseth@gmail.com</h3>
-      <div className="buttons">
-        <button className="email">
-          <i className="fa-solid fa-envelope" id="email-envelope"></i>Email
-        </button>
-        <button className="linkedin">
-          <i className="fa-brands fa-square-linkedin" id="linkedin-in"></i>
-          linkedIn
-        </button>
+    <>
+      <div>
+        <img src={seth} alt="profile picture" class="profile-pic" />
       </div>
-    </div>
+      <div className="top-half">
+        <br />
+        <h1>Seth Johnson</h1>
+        <h2>FullStack Developer</h2>
+        <h3>samaritanbrotherseth@gmail.com</h3>
+        <div className="buttons">
+          <button className="email">
+            <i className="fa-solid fa-envelope" id="email-envelope"></i>Email
+          </button>
+          <button className="linkedin">
+            <i className="fa-brands fa-square-linkedin" id="linkedin-in"></i>
+            linkedIn
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
